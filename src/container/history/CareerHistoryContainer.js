2df25f4e3ec79e8c3d4db28b499a530e96382d15
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-09 02:28:08 +0900

    기본 골격 작성중

--- src/container/history/CareerHistoryContainer.js
+++ src/container/history/CareerHistoryContainer.js
@@ -1,7 +1,18 @@
 export default () => {
     return (
         <>
-            <div>dd</div>
+            <div className="row">
+                <div className="col-12 my-2">
+                    <div className="card p-5">
+                        <h3>Projects</h3>
+                    </div>
+                </div>
+                <div className="col-12 my-2">
+                    <div className="card p-5">
+                        <h3>Timeline</h3>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/container/profile/ProfileContainter.js
+++ src/container/profile/ProfileContainter.js
@@ -4,21 +4,45 @@ export default () => {
     return (
         <>
             <div className="row">
-                <div className="col-md-4 my-1">
-                    <div className="card">
+                <div className="col-md-4 my-2 d-flex align-items-stretch">
+                    <div className="card w-100">
                         <div className="d-flex flex-column align-items-center text-center py-5">
                             <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
 
                             <h2 className="fw-normal">윤주현</h2>
-                            <p>ㅇㅇ</p>
-                            <p><a className="btn btn-secondary" href="#">View details &raquo;</a><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+                            <p>자기소개</p>
+                            <p>
+                                <a className="btn btn-outline-dark m-1">Github</a>
+                                <a className="btn btn-outline-primary m-1">Tistory</a>
+                                <a className="btn btn-outline-danger m-1">Instagram</a>
+                            </p>
 
                         </div>
                     </div>
                 </div>
-                <div className="col-md-8 my-1">
-                    <div className="card">
+                <div className="col-md-8 my-2 d-flex align-items-stretch">
+                    <div className="card p-5 w-100">
                         <h3>개인정보</h3>
+                        <div>
+                            <ul>
+                                <li>생년월일</li>
+                                <li>ㅇㅇ</li>
+                                <li>ㅇㅇ</li>
+                                <li>ㅇㅇ</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 my-2">
+                    <div className="card p-5">
+                        <h3>Tech Stack</h3>
+                    </div>
+                </div>
+                <div className="col-12 my-2">
+                    <div className="card p-5">
+                        <h3>Curriculum</h3>
                     </div>
                 </div>
             </div>
