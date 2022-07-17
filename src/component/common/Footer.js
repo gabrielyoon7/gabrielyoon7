21c7de4882d583ee87f066d313e4957920678d72
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-17 20:39:28 +0900

    색상 개선

--- src/component/common/Footer.js
+++ src/component/common/Footer.js
@@ -1,14 +1,12 @@
 export default () => {
-    return (
-        <footer className="py-3 my-4">
-        {/* <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
-        </ul> */}
-        <p className="text-center text-muted"><a href="https://github.com/gabrielyoon7">&copy; 2022 Gabriel Ju Hyun, Yoon</a></p>
-      </footer>
-    )
+  return (
+    <footer className="py-3 my-4">
+      <p className="text-center text-muted"><a href="#" onClick={() => {
+        window.open(
+          'https://github.com/gabrielyoon7',
+          '_blank'
+        )
+      }}>&copy; 2022 Gabriel Ju Hyun, Yoon</a></p>
+    </footer>
+  )
 }
