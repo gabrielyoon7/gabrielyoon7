d272e03ffa6f7ba56c328f045c4c79e591e81b42
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-26 14:56:06 +0900

    커리큘럼 추가

---
+++ src/component/Table/index.jsx
@@ -0,0 +1,46 @@
+// @/src/components/Table/index.jsx
+import React, { useState } from "react";
+
+import useTable from "../../hooks/useTable";
+import styles from "./Table.module.css";
+import TableFooter from "./TableFooter";
+
+const Table = ({ data, rowsPerPage }) => {
+  const [page, setPage] = useState(1);
+  const { slice, range } = useTable(data, page, rowsPerPage);
+  return (
+    <>
+      <table className={styles.table}>
+        <thead className={styles.tableRowHeader}>
+          <tr>
+            <th className={styles.tableHeader}>년도 학기</th>
+            <th className={styles.tableHeader}>학수코드</th>
+            <th className={styles.tableHeader}>교과목명</th>
+            <th className={styles.tableHeader}>이수구분</th>
+            <th className={styles.tableHeader}>인증구분</th>
+            <th className={styles.tableHeader}>학점</th>
+            <th className={styles.tableHeader}>설계학점</th>
+            <th className={styles.tableHeader}>등급</th>
+          </tr>
+        </thead>
+        <tbody>
+          {slice.map((el) => (
+            <tr className={styles.tableRowItems} key={el.id}>
+              <td className={styles.tableCell}>{el.year}</td>
+              <td className={styles.tableCell}>{el.id}</td>
+              <td className={styles.tableCell}>{el.title}</td>
+              <td className={styles.tableCell}>{el.type1}</td>
+              <td className={styles.tableCell}>{el.type2}</td>
+              <td className={styles.tableCell}>{el.point}</td>
+              <td className={styles.tableCell}>{el.design_point}</td>
+              <td className={styles.tableCell}>{el.grade}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
+    </>
+  );
+};
+
+export default Table;
