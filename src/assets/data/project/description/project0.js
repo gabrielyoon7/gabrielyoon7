069aa090b9c1b1879712b05ba8e2c56d15888a8f
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-08-09 00:05:54 +0900

    이미지 경로 버그 수정 (markdown 경로 문제)

--- src/assets/data/project/description/project0.js
+++ src/assets/data/project/description/project0.js
@@ -1,6 +1,6 @@
 const long_description = `
 # 제목제목?
-![App Screenshot](assets/img/me.jpg)
+![App Screenshot](project_img/0/me.jpg)
 C:\web_intellij\gabrielyoon7\src\assets\img\me.jpg
 src\assets\img\me.jpg
 ![A medium cat image](https://cdn2.thecatapi.com/images/9qLSHCaQQ.jpg)
