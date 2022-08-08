83e352485ea1970273195d001bf3d8bc5f9527a9
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-08-08 15:11:10 +0900

    기술표 업데이트

---
+++ src/assets/data/tech.js
@@ -0,0 +1,25 @@
+
+const tech = [
+    { type:"fe", label: "React", value: "react", bgColor:"#004080", txtColor:'#FFFFFF'},
+    { type:"fe", label: "React Natvie", value: "react-native", bgColor:'#004080', txtColor:'#FFFFFF'},
+    { type:"fe", label: "Native Base", value: "native-base", bgColor:"#0080c0", txtColor:'#FFFFFF'},
+    { type:"fe", label: "Bootstrap", value: "bootstrap", bgColor:"#8000ff", txtColor:'#FFFFFF'},
+    { type:"be", label: "Node.js", value: "node-js", bgColor:'#2F9D27', txtColor:'#FFFFFF'},
+    { type:"db", label: "Mongo DB", value: "mongo-db", bgColor:'#2F9D27', txtColor:'#FFFFFF'},
+    { type:"db", label: "MySQL", value: "mysql", bgColor:"#0080c0", txtColor:'#FFFFFF'},
+    { type:"db", label: "MariaDB", value: "mariadb", bgColor:"#0080c0", txtColor:'#FFFFFF'},
+    { type:"be", label: "Express.js", value: "express-js", bgColor:'#F6F6F6', txtColor:'#000000'},
+    { type:"be", label: "Java", value: "java", bgColor:'#FF0000', txtColor:'#FFFFFF'},
+    { type:"fe", label: "Python", value: "python", bgColor:"#0080c0", txtColor:'#FFFFFF'},
+    { type:"be", label: "JSP", value: "jsp", bgColor:'#FF0000', txtColor:'#FFFFFF'},
+    { type:"fe", label: "JavaScript", value: "javascript", bgColor:'#FFBB00', txtColor:'#FFFFFF'},
+    { type:"fe", label: "jQuery", value: "jquery", bgColor:'#FFBB00', txtColor:'#FFFFFF'},
+    { type:"fe", label: "CSS", value: "css", bgColor:"#0080ff", txtColor:'#FFFFFF'},
+    { type:"fe", label: "HTML", value: "html", bgColor:"#ff8000", txtColor:'#FFFFFF'},
+    { type:"fe", label: "Expo", value: "expo", bgColor:"#000000", txtColor:'#FFFFFF'},
+    { type:"etc", label: "Tomcat", value: "tomcat", bgColor:"#000000", txtColor:'#FFFFFF'},
+    { type:"etc", label: "Ubuntu", value: "ubuntu", bgColor:"#000000", txtColor:'#FFFFFF'},
+    { label: "??", value: "??", bgColor:"", txtColor:'#FFFFFF', disabled: true },
+]
+
+export default tech;
--- src/container/profile/TechStack.js
+++ src/container/profile/TechStack.js
@@ -1,44 +1,31 @@
+import tech from "../../assets/data/tech"
+
 export default () => {
-    const tech = [
-        { type: "fe", name: "React", color: "primary" },
-        { type: "fe", name: "JavaScript", color: "warning" },
-        { type: "fe", name: "CSS", color: "info" },
-        { type: "fe", name: "HTML", color: "danger" },
-        { type: "fe", name: "Expo", color: "dark" },
-        { type: "fe", name: "React Native", color: "primary" },
-        { type: "fe", name: "Native Base", color: "info" },
-        { type: "be", name: "Java", color: "danger" },
-        { type: "be", name: "JSP", color: "dark" },
-        { type: "fe", name: "jQuery", color: "secondary" },
-        { type: "db", name: "MongoDB", color: "success" },
-        { type: "be", name: "Node.js", color: "success" },
-        { type: "be", name: "express.js", color: "light" },
-        { type: "be", name: "Python", color: "primary" },
-        { type: "db", name: "MySQL", color: "primary" },
-        { type: "db", name: "MariaDB", color: "info" },
-        { type: "fe", name: "bootstrap", color: "light" },
-        { type: "etc", name: "Ubuntu", color: "danger" },
-        { type: "etc", name: "Tomcat", color: "warning" },
-    ]
+    const findTech = (t) => {
+        const idx = tech.findIndex((tech)=>tech.value===t.value)
+        return tech[idx]
+    }
+    // console.log(tech)
+
     return (
         <>
             <h3>Tech Stack</h3>
             <div className="row">
                 <div className="col-md-4 my-1">
                     <h5>Front-End</h5>
-                    {tech.filter((t) => t.type === 'fe').map((t) => <span key={t.name} className={"me-1 badge text-bg-" + t.color}>{t.name}</span>)}
+                    {tech.filter((t) => t.type === 'fe').map((t)=><span key={t.value} className="badge me-1" style={{"backgroundColor":findTech(t).bgColor, "color":findTech(t).txtColor}}>{findTech(t).label}</span>)}
                 </div>
                 <div className="col-md-4 my-1">
                     <h5>Back-End</h5>
-                    {tech.filter((t) => t.type === 'be').map((t) => <span key={t.name} className={"me-1 badge text-bg-" + t.color}>{t.name}</span>)}
+                    {tech.filter((t) => t.type === 'be').map((t)=><span key={t.value} className="badge me-1" style={{"backgroundColor":findTech(t).bgColor, "color":findTech(t).txtColor}}>{findTech(t).label}</span>)}
                 </div>
                 <div className="col-md-4 my-1">
                     <h5>DBMS</h5>
-                    {tech.filter((t) => t.type === 'db').map((t) => <span key={t.name} className={"me-1 badge text-bg-" + t.color}>{t.name}</span>)}
+                    {tech.filter((t) => t.type === 'db').map((t)=><span key={t.value} className="badge me-1" style={{"backgroundColor":findTech(t).bgColor, "color":findTech(t).txtColor}}>{findTech(t).label}</span>)}
                 </div>
                 <div className="col">
                     <h5>Others</h5>
-                    {tech.filter((t) => t.type === 'etc').map((t) => <span key={t.name} className={"me-1 badge text-bg-" + t.color}>{t.name}</span>)}
+                    {tech.filter((t) => t.type === 'etc').map((t)=><span key={t.value} className="badge me-1" style={{"backgroundColor":findTech(t).bgColor, "color":findTech(t).txtColor}}>{findTech(t).label}</span>)}
                 </div>
             </div>
         </>
