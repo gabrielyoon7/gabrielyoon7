33d919088a3015b0ad30b7a4ba3e175be8608999
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-08-08 23:55:22 +0900

    이미지 오류 수정중

--- src/assets/data/project/description/project0.js
+++ src/assets/data/project/description/project0.js
@@ -1,7 +1,8 @@
 const long_description = `
-# 인공지능전공 홈페이지 작업을 했음ㅇㅇ
+# 제목제목?
 ![App Screenshot](assets/img/me.jpg)
 C:\web_intellij\gabrielyoon7\src\assets\img\me.jpg
 src\assets\img\me.jpg
+![A medium cat image](https://cdn2.thecatapi.com/images/9qLSHCaQQ.jpg)
 `
 export default long_description;
