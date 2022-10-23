cdcad0580ebac3f3e34ea6b8d9dffb17fda4aa45
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-23 17:56:32 +0900

    커리어 페이지 수정중

--- src/containers/history/CareerHistoryContainer.js
+++ src/containers/history/CareerHistoryContainer.js
@@ -1,21 +1,31 @@
+import { Grid, Typography } from "@mui/material"
+import HomeCard from "components/common/home/HomeCard"
 import Projects from "./Projects"
 
 export default () => {
     return (
         <>
-            <div className="row">
-                <div className="col-12 my-2">
-                    <div className="card p-5 mb-4 rounded-4 border-primary border bg-white">
-                        <h3>Projects</h3>
-                        <Projects/>
-                    </div>
-                </div>
-                <div className="col-12 my-2">
-                    <div className="card p-5 mb-4 rounded-4 border-primary border bg-white">
-                        <h3>Timeline</h3>
-                    </div>
-                </div>
-            </div>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <HomeCard
+                        children={<>
+                            <Typography variant="h3">
+                                Projects
+                            </Typography>
+                            <Projects />
+                        </>}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <HomeCard
+                        children={<>
+                            <Typography variant="h3">
+                                Timeline
+                            </Typography>
+                        </>}
+                    />
+                </Grid>
+            </Grid>
         </>
     )
 }
