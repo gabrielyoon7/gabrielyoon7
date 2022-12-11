b63834873abe88cf856300fe0f36106d6c53aeee
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-12-12 01:34:14 +0900

    feat: project 3 개선

--- src/assets/data/project/description/project3.js
+++ src/assets/data/project/description/project3.js
@@ -1,11 +1,13 @@
 const long_description = `
-### [캡스톤 차세대 일기 애플리케이션]
+### ■▮ 캡스톤 차세대 일기 애플리케이션 (팀장) ▮■
 
-3학년을 마친 시점에서 무작정 React를 학습하기 시작했습니다. 사실 훨씬 전부터 React에 대해서 가볍게 (맛보는 수준으로) 학습을 해왔지만, 정말 제대로 해봐야겠다고 결심을 한 때는 기존에 맡았던 학과 홈페이지 관리팀을 그만두고부터였습니다. 
+---
 
-캡스톤 강좌를 통해 졸업 작품을 준비하게 됐는데, React보다 React Native를 먼저 깊게 사용하게 됐습니다. React, React Native, Node.js, Express.js, MongoDB를 사용하였습니다. 
+* 3학년을 마친 시점에서 무작정 React를 학습하기 시작했습니다. 사실 훨씬 전부터 React에 대해서 가볍게 (맛보는 수준으로) 학습을 해왔지만, 정말 제대로 해봐야겠다고 결심을 한 때는 기존에 맡았던 학과 홈페이지 관리팀을 인수인계 하고부터였습니다. 
 
-차세대 일기 애플리케이션으로 일기를 태그로 분석해주는 기능 등을 구현하고자 하였습니다.
+* 교내 졸업 작품을 만드는 캡스톤 강좌를 통해 졸업 작품을 준비하게 됐는데, 특이하게도 React보다 React Native를 먼저 깊게 사용하게 됐습니다. 이 프로젝트에서는 React, React Native, Bootstrap, Native Base, Node.js, express.js, MongoDB, mongoose.js를 사용하였습니다. 
+
+* 차세대 일기 애플리케이션으로 일기를 태그로 분석해주는 기능 등을 구현하고자 하였습니다.
 
 ![App Screenshot](project_img/3/app_main.gif)
 ![App Screenshot](project_img/3/app_sns.gif)
@@ -15,6 +17,9 @@ const long_description = `
 ![App Screenshot](project_img/3/app_follow.gif)
 ![App Screenshot](project_img/3/web_read.gif)
 ![App Screenshot](project_img/3/web_write.gif)
+
+* 이 프로젝트에서는 React의 기초에 대해서 학습할 수 있는 기회가 됐습니다.
+* 많은 시행 착오 끝에, React가 어떤 방식으로 동작하는지 학습하게 됐습니다.
 
 `;
 export default long_description;
