35165580b40824bd8b7499083114e4d3ee830834
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-12-12 01:45:17 +0900

    feat: project 5 내용 수정

--- src/assets/data/project/description/project5.js
+++ src/assets/data/project/description/project5.js
@@ -1,9 +1,40 @@
 const long_description = `
 
-현재 진행중인 React Node.js Express Mongo DB 프로젝트입니다.
+## ■▮ 그룹 활동 지원 서비스 및 채용 연계 플랫폼 ▮■
+---
 
-경기대학교 소프트웨어중심대학 SW 경진대회 참가중
+### ■ 경기대학교 2022-2 SW상상기업 팀 아보카도 프로젝트 (팀장)
 
-(11월 프로젝트 종료 이후 수정 예정)
+* 2021년 부터 교내 SW 경진 대회인 SW상상기업에 참가하였습니다.
+* 당시에 학과홈페이지 인수인계를 목적으로 팀원을 모집했던 후배들에게 제가 1년 간 학습한 Javascript와 React에 관한 지식을 나누기 위해 이 프로젝트를 시작했습니다.
+* 제 스스로 그동안 React 보다 React Native를 개발을 해왔기에, 이번 기회에 React 환경에서도 개발 경험을 쌓기를 희망했던 부분도 있습니다.
+* 실제로 해보고 싶었던 기능들을 사용해보기도 했고, 마치 연습장 처럼 React를 학습할 수 있는 기회였습니다.
+
+![App Screenshot](project_img/5/01main.png)
+![App Screenshot](project_img/5/02signin.png)
+![App Screenshot](project_img/5/03groupfinder.png)
+![App Screenshot](project_img/5/03groupfinder.png)
+![App Screenshot](project_img/5/04groupcreator.png)
+![App Screenshot](project_img/5/05groupsearch.png)
+![App Screenshot](project_img/5/06groupenlist.png)
+![App Screenshot](project_img/5/07organizations.png)
+![App Screenshot](project_img/5/08organizationscreator.png)
+![App Screenshot](project_img/5/09grouporganizationenter.png)
+![App Screenshot](project_img/5/10grouporganizationfinder.png)
+![App Screenshot](project_img/5/11myworkspace.png)
+![App Screenshot](project_img/5/12readmeshort.png)
+![App Screenshot](project_img/5/13workspacereadme.png)
+![App Screenshot](project_img/5/14workspacecalendar.png)
+![App Screenshot](project_img/5/15workspacenotice.png)
+![App Screenshot](project_img/5/16workspacefunding.png)
+![App Screenshot](project_img/5/17workspacesettings.png)
+![App Screenshot](project_img/5/18workspacerating.png)
+![App Screenshot](project_img/5/19profile.png)
+![App Screenshot](project_img/5/20hr.png)
+![App Screenshot](project_img/5/21premium.png)
+![App Screenshot](project_img/5/22jobenlist.png)
+![App Screenshot](project_img/5/23jobfinder.png)
+![App Screenshot](project_img/5/24jobfindermodal.png)
+
 `;
 export default long_description;
