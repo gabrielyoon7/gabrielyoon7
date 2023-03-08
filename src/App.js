9d4f2d8f13d9708b0c1dda16b4e26d1712ad2631
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-08 12:15:34 +0900

    feat: tailwind 예제 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Example from './Example';
+import HockeyTeamList from './HockeyTeamList';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Example />
+      <HockeyTeamList />
+    </>
   );
 }
 
